044455cfa78e960e5a5d31329c2c5ea041d54903
ljsjack <lowjiansheng@hotmail.sg>
2016-09-10 14:54:19 +0800

    Add routing and Schema file to server.

---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+// Dependencies
+
+var koa = require('koa');
+app = koa();
+var router = require('koa-router')();
+var entry = require()
+
+/**
+ * GET the whole database
+ */
+router.get('/', function* (){
+    this.body = 'test';
+});
+
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,11 +1,46 @@
 var Koa = require('koa');
 var serve = require('koa-static');
 var connections = require('./config/connections');
+// var bodyParser = require('body-parser');
 
 var app = Koa();
 var url = connections.hostname + ':' + connections.port;
 
+/**
+ * Connection to database
+ */
+var mongoose = require('mongoose');
+var dbConnection = mongoose.connect('mongodb://localhost/myApp',
+    function (err) {
+        if (err) {
+            console.log('Connection Error,err');
+        }
+        else {
+            console.log('Connection Successful');
+        }
+    });
+
+
+/**
+ * Body Parser to parse the json object
+
+app.use(bodyParser.urlencoded({ extended:true}));
+app.use(bodyParser.json());
+*/
+
+
+/**
+ * Routings
+ */
+var routing = require('./routes/routes');
+app.use(routing.routes());
+app.use(routing.allowedMethods());
+
+/**
+ * Starts the server and push users to the public page.
+ */
 app.use(serve(__dirname + '/public'));
 app.listen(connections.port);
 
 console.log('Serving on ' + url + ' in ' + connections.env);
+
