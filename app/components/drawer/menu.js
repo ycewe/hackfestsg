0d1f27b6fa2d8b0188eb7945dd0e6fda982dfef5
Eric Ewe <eric.ewe@zalora.com>
2016-09-10 15:14:51 +0800

    add drawer menu icons and tabs

--- app/components/drawer/menu.js
+++ app/components/drawer/menu.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class Menu extends React.Component {
   render() {
     return (
       <nav id="drawer">
-        <ul id="drawer-menu">
-          <li>Home</li>
-          <li>Search</li>
-          <li>Save</li>
-          <li>Upload</li>
-        </ul>
+          <ul id="drawer-menu">
+            <Link to={`/home`} style={style} activeStyle={activeStyle}>
+              <li><img src="./resources/images/Icons/home.png" alt="home" className="menu-icon"/>Home</li>
+            </Link>
+            <Link to={`/search`} style={style} activeStyle={activeStyle}>
+              <li><img src="./resources/images/Icons/search.png" alt="home" className="menu-icon"/>Search</li>
+            </Link>
+            <Link to={`/saved`} style={style} activeStyle={activeStyle}>
+              <li><img src="./resources/images/Icons/saved.png" alt="home" className="menu-icon"/>Saved</li>
+            </Link>
+            <Link to={`/upload`} style={style} activeStyle={activeStyle}>
+              <li><img src="./resources/images/Icons/upload.png" alt="home" className="menu-icon"/>Upload</li>
+            </Link>
+          </ul>
       </nav>
     );
   }
+}
+
+const style = {
+  color: 'black',
+  textDecoration: 'none',
+}
+
+const activeStyle = {
+  color: 'red',
+  textDecoration: 'none',
 }
 
 export default Menu;
