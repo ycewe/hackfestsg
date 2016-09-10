0f96e60b7e9c92ecc67069b9e74b75743a0cca44
Eric Ewe <eric.ewe@zalora.com>
2016-09-10 14:35:13 +0800

    add content to drawer menu

--- app/components/drawer/menu.js
+++ app/components/drawer/menu.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const Menu = () => (
-  <nav id="drawer"></nav>
-);
+class Menu extends React.Component {
+  render() {
+    return (
+      <nav id="drawer">
+        <ul id="drawer-menu">
+          <li>Home</li>
+          <li>Search</li>
+          <li>Save</li>
+          <li>Upload</li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 export default Menu;
-
